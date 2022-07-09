39647ef984e6fda8dbfcc300d2fe0f550d1462cd
Abdullah7771 <65004459+Abdullah7771@users.noreply.github.com>
2022-07-09 22:31:18 +0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,5 @@
 echo "Hello"
 get-help
-Get-Command -ListImported
 Get-Command -Type Cmdlet | Sort-Object -Property Noun | Format-Table -GroupBy Noun
 Get-Command -Module Microsoft.PowerShell.Security, Microsoft.PowerShell.Utility
  git --version
