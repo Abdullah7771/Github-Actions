a0ea29c2ad5a8ec95774d5e8a9c2f2b0aabdbb4f
Abdullah7771 <65004459+Abdullah7771@users.noreply.github.com>
2022-07-09 21:56:50 +0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,2 +1,8 @@
 echo "Hello"
 get-help 
+Get-Service
+Get-Service | Where-Object {$_.status -eq "stopped"}
+PS C:\Users\babug> get-alias | convertto-html > aliases.htm
+PS C:\Users\babug> invoke-item aliases.htm
+history
+clear
